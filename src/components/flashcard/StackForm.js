1f9a13fee4791cc02121bee26e7fc0e2c6c0f746
viet <viettangle12@gmail.com>
2017-11-25 22:35:09 -0500

    add styling to flashcar

--- src/components/flashcard/StackForm.js
+++ src/components/flashcard/StackForm.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Form from "muicss/lib/react/form";
+import Button from "muicss/lib/react/button";
+import Input from "muicss/lib/react/input";
 
 class StackForm extends Component {
-  render = () => {
-      return(
-          <div>
-              <Link to="/flashcard"><h4>Flashcard</h4></Link>
-              <h4>Create a New Stack</h4>
-          </div>
-      );
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      cards: []
+    };
   }
+
+  addCard = () => {
+    const { cards } = this.state;
+    cards.push({id: cards.length, prompt: '', anwser: ''});
+    this.setState({cards})
+  };
+
+  render = () => {
+      console.log('state:',this.state);
+    return (
+      <div>
+        <Link to="/flashcard">
+          <h4>Flashcard</h4>
+        </Link>
+        <div className="mui--text-center">
+          <h4>Create a New Stack</h4>
+          <br />
+          <Form inline={true}>
+            <Input id="title" label="Title" floatingLabel={true} />
+          </Form>
+          <br />
+          <Button id="submitStack" variant="raised" onClick={this.addCard}>
+            Submit
+          </Button>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default StackForm;
